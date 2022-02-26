de94ecb3fcda98e6cf16af73aa92eed7221dd7e6
Bafana <64266129+Bafana-tech@users.noreply.github.com>
2022-02-26 21:37:19 +0200

    wrote client.py to allow making instances

--- src/client.py
+++ src/client.py
@@ -1,19 +1,42 @@
 from socket import *
 
-serverName = '127.0.0.1' # IP adress of server
-serverPort = 40506      # Port number
+class client:
 
-clientSocket = socket(AF_INET, SOCK_DGRAM) #Creating UDP socket
+	#clientSocket = 0
+
+	# Constructor
+	def __init__ (self, serverName, serverPort):
+		self.serverName = serverName
+		self.serverPort = serverPort
 
 
-message = raw_input('input lowercase sentence:\n') #Taking message from client
+	# creating UDP socket
+	def udpSocket(self):
+		# making clientSocket global
+		global clientSocket
+		clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-# send the message to server
-clientSocket.sendto(message.encode() , (serverName, serverPort))
 
-#read the message from server
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+	def messages(self):
+		message = raw_input('input lowercase sentence\n')
 
-print(modifiedMessage.decode())
-clientSocket.close()
+		clientSocket.sendto(message.encode(), (self.serverName, self.serverPort))
+		print(self.serverPort)
 
+	def receives(self):
+		print("receiving....")
+		modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+
+		#print("from server", end='')
+		print(modifiedMessage)
+		
+		#clientSocket.close()
+
+def main():
+	client1 = client('127.0.0.1', 40506)
+	client1.udpSocket()
+	client1.messages()
+	client1.receives()
+	#print(client1)
+
+main()
--- src/server.py
+++ src/server.py
@@ -1,21 +1,22 @@
 from socket import *
 
-serverPort = 40506
+class server:
+	serverPort = 40506
 
-# Creating a UDP socket
-serverSocket = socket(AF_INET, SOCK_DGRAM)
+	# Creating a UDP socket
+	serverSocket = socket(AF_INET, SOCK_DGRAM)
 
-# binding socket to local port number 405060
-serverSocket.bind(("", serverPort))
+	# binding socket to local port number 405060
+	serverSocket.bind(("", serverPort))
 
-print("server started...")
+	print("server started...")
 
-while True:
-	# read UDP socket into message
-	message, clientAddress = serverSocket.recvfrom(2048)
+	while True:
+		# read UDP socket into message
+		message, clientAddress = serverSocket.recvfrom(2048)
 
-	modifiedMessage = message.decode().upper()
+		modifiedMessage = message.decode().upper()
 
-	# send upper case string back to this client
-	serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+		# send upper case string back to this client
+		serverSocket.sendto(modifiedMessage.encode(), clientAddress)
 
